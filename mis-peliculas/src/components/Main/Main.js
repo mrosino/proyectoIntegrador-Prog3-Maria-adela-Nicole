bf3b9f38e3bc500fe8f157ec5adb2ee57d635ab4
mrosino <mrosino@udesa.edu.ar>
2021-09-08 12:31:11 -0300

    probando fetch de genero

--- mis-peliculas/src/components/Main/Main.js
+++ mis-peliculas/src/components/Main/Main.js
@@ -7,11 +7,12 @@ class Main extends Component {
     super(props);
     this.state = {
       movies: [],
+      genre : [],
     };
 
   }
 componentDidMount(){
-  fetch("https://api.themoviedb.org/3/movie/popular?api_key=35c3a4bec2a3c008c9fa7737b86aadc1&language=en-US&page=7")
+  fetch("https://api.themoviedb.org/3/movie/popular?api_key=35c3a4bec2a3c008c9fa7737b86aadc1&language=en-US&page=1")
   .then(response => response.json())
   .then(movies => {
     this.setState({
@@ -21,7 +22,15 @@ componentDidMount(){
     console.log(movies);
 
   }) 
-
+  fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=35c3a4bec2a3c008c9fa7737b86aadc1&language=en-US")
+  .then(response => response.json())
+  .then (genre => {
+    this.setState ({
+      genre: genre.results
+    })
+    console.log(genre);
+  })
+ 
 }
   render = () => {
     //poner loader
