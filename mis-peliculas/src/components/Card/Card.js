26a07cb102f4937b1fad62a7c6b7fcd2277cd740
mrosino <mrosino@udesa.edu.ar>
2021-09-09 21:55:21 -0300

    pruebas con css

--- mis-peliculas/src/components/Card/Card.js
+++ mis-peliculas/src/components/Card/Card.js
@@ -10,16 +10,25 @@ render (){
 
   return (
     <React.Fragment>
+      <article className="card">
           <h3>{this.props.title}</h3>
-          <img src= {"https://image.tmdb.org/t/p/w342/" + this.props.poster_path} alt="" />
+          <img className="imagen" src= {"https://image.tmdb.org/t/p/w342/" + this.props.poster_path} alt="" />
+          <div> 
           <p className="description"> Descripcion: {this.props.overview}</p>
-          <section className="aditional-info">
-            <p>Genero {this.props.genre}</p>
+          </div>
+          <div className="aditional-info">
+          <p>Genero {this.props.genre}</p>
             <p>Rating {this.props.vote_average}</p>
             <p>{this.props.adult === false ? "ATP": "No ATP"}  </p>
+          </div>
         
-          </section>
+          
+            
+         
+         
+          
           <a href="#">Ver más</a>
+          </article>
    
     </React.Fragment>
   );
